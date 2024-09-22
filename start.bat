8abdb496389d3a64217e53c1e1b9766a60014f6d
Saahil-04 <digital.saahilsvishwakarma@gmail.com>
2024-09-22 12:58:56 +0530

    first commit

---
+++ start.bat
@@ -0,0 +1,17 @@
+@echo off
+
+rem Start the frontend without opening a new terminal window
+cd D:\summarizerAI\frontend
+start "" /B npm start dev
+
+rem Start the backend without opening a new terminal window
+cd D:\summarizerAI\backend
+start "" /B npm start run dev
+
+rem Start the fastapi without opening a new terminal window
+cd D:\summarizerAI\python
+call .\env\Scripts\activate
+start "" /B  uvicorn main:app --reload
+
+rem Exit the script
+exit
