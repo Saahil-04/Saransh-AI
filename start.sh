283cb50fd193fd37d6bedffc4873bcf8f7ef63ce
Saahil-04 <digital.saahilsvishwakarma@gmail.com>
2024-09-22 13:31:23 +0530

    README

---
+++ start.sh
@@ -0,0 +1,10 @@
+# #!/bin/bash
+
+# # Start the frontend
+# cd frontend
+# npm start dev &
+
+# # Activate the Python environment and start the backend
+# cd ../python
+# source ./env/Scripts/activate
+# uvicorn main:app --reload
