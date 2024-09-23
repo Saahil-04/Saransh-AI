abfab6ecd5394bc86170fa45aaafaa8c4b915cec
Saahil-04 <digital.saahilsvishwakarma@gmail.com>
2024-09-23 12:50:59 +0530

    Batch file

--- start.bat
+++ start.bat
@@ -1,15 +1,15 @@
 @echo off
 
 rem Start the frontend without opening a new terminal window
-cd D:\summarizerAI\frontend
+cd D:\Saransh-AI\frontend
 start "" /B npm start dev
 
 rem Start the backend without opening a new terminal window
-cd D:\summarizerAI\backend
+cd D:\Saransh-AI\backend
 start "" /B npm start run dev
 
 rem Start the fastapi without opening a new terminal window
-cd D:\summarizerAI\python
+cd D:\Saransh-AI\python
 call .\env\Scripts\activate
 start "" /B  uvicorn main:app --reload
 
